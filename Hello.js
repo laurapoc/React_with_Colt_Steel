b54ae7a11dac847f2c06797042de4b38aea7f1e8
laurapoc <laura.pociuniene@gmail.com>
2021-02-04 18:44:11 +0200

    immutable props very basics

--- Hello.js
+++ Hello.js
@@ -1,11 +1,12 @@
 /*jshint esversion: 6 */
 
 class Hello extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Hello there!</h1>
-        </div>
-      );
-    }
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h1>Hi {this.props.to} from {this.props.from}!</h1>
+      </div>
+    );
   }
+}
