99f7a7cd81f445b67953ac68ba3bdb1f1f6f3d6a
laurapoc <laura.pociuniene@gmail.com>
2021-02-04 17:53:12 +0200

    JSX Conditionals

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 /*jshint esversion: 6 */
-
-const getMood = () => {
-  const moods = ["Angry", "Hungry", "Silly", "Quiet", "Paranoid"];
-  return moods[Math.floor(Math.random() * moods.length)];
+const getNum = () => {
+  return Math.floor(Math.random() * 10) + 1;
 };
 
-class JSXDemo extends React.Component {
+class NumPicker extends React.Component {
   render() {
+    const num = getNum();
+    
     return (
       <div>
-        <h1>My image!</h1>
-        <h3>My number is: {2 * 8.4}</h3>
-        <h3>My current mood is: {getMood()}</h3>
-        <img src="https://images.unsplash.com/photo-1611846043818-e4186a5a494e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80" />
+        <h1>Your number is {num}</h1>
+        <p>{num === 7 ? "Congrats!" : "Unlucky..."}</p>
+        {num === 7 && <img src="https://media.giphy.com/media/Qv5JV60U5Qm26Ipp9J/giphy.gif" />}
       </div>
     );
   }
 }
 
-ReactDOM.render(<JSXDemo />, document.getElementById("root"));
+ReactDOM.render(<NumPicker />, document.getElementById("root"));
