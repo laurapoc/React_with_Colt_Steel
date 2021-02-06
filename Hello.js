e031fedd1f4a475cb78723b9b1559b31ba8a4ed2
laurapoc <laura.pociuniene@gmail.com>
2021-02-06 16:49:05 +0200

    setting default props

--- Hello.js
+++ Hello.js
@@ -1,13 +1,20 @@
 /*jshint esversion: 6 */
 
 class Hello extends React.Component {
+  static defaultProps = {
+    from: "Anonymous",
+    bangs: 1
+  };
   render() {
     let bangs = "!".repeat(this.props.bangs);
     return (
       <div>
         <h1>
-          <p>Hi {this.props.to} from {this.props.from}{bangs}</p>
-          <img src={this.props.img}/>
+          <p>
+            Hi {this.props.to} from {this.props.from}
+            {bangs}
+          </p>
+          <img src={this.props.img} />
         </h1>
       </div>
     );
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ class App extends React.Component {
     return (
       <div>
         <Hello to="Ringo" from="Paul" bangs={4}  img="https://images.unsplash.com/photo-1612288528103-edc64749d4ad?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"/>
+        <Hello to="George" />
       </div>
     );
   }
