9b8be6d11fa2957d6fa7f2571df6972008804469
laurapoc <laura.pociuniene@gmail.com>
2021-02-06 16:40:40 +0200

    looping in jsx basics

---
+++ Friend.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+class Friend extends React.Component {
+  render() {
+    const { name, hobbies } = this.props;
+    return(
+      <div>
+        <h1>{name}</h1>
+        <ul>
+         {hobbies.map(h => <li>{h}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -3,7 +3,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello to="Ringo" from="Paul" bangs={4}  img="https://images.unsplash.com/photo-1612288528103-edc64749d4ad?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"/>
+        <Friend
+        name="Elton"
+        hobbies={["Piano", "Singing", "Dancing"]} 
+        />
+        <Friend
+        name="Frida"
+        hobbies={["Drawing", "Painting", "Reading"]} 
+        />
       </div>
     );
   }
